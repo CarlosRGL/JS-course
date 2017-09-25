8744885d83cbace2848b3462cc174ad5a8aaa916
CarlosRGL <carlos@quai13.com>
2017-09-25 16:47:47 +0200

    this keyword look up

--- 02 How-JS-Works/script.js
+++ 02 How-JS-Works/script.js
@@ -3,48 +3,51 @@
 
 // functions
 
-// This works because calculageAge() is a funcion declaration and is stored in the variable objet
+// This works because calculageAge() is a funcion declaration and is stored in the variable object
 calculageAge(1988);
 
 function calculateAge(year) {
   console.log(2016 - year);
 }
 
-
-// this dont work because is a function expression and isnt stored in the variable objet
+// this dont work because is a function expression and isnt stored in the variable object
 retirement(1990);
 
 var retirement = function(year) {
   console.log(65 - (2016 - year));
 };
 
-
 //variables
 
-console.log(age);  // this output 'undifined' because JS store variables in the variable objet as undefined;
+console.log(age); // this output 'undifined' because JS store variables in the variable object as undefined;
 var age = 28;
-console.log(age);
+console.log(age); //28
 
+function foo() {
+  var age = 65;
+  console.log(age);
+}
+
+foo(); //65
+console.log(age); //28
 
 ///////////////////////////////////////
 // Lecture: Scoping
 
 // First scoping example
 
-/*
-var a = 'Hello!';
+var a = "Hello!";
 first();
 
 function first() {
-    var b = 'Hi!';
-    second();
+  var b = "Hi!";
+  second();
 
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
+  function second() {
+    var c = "Hey!";
+    console.log(a + b + c);
+  }
 }
-*/
 
 // Example to show the differece between execution stack and scope chain
 
@@ -70,3 +73,35 @@ function third() {
 
 ///////////////////////////////////////
 // Lecture: The this keyword
+
+// the this variable is only assigned a value when the objetct calls the method
+
+console.log(this); // Output the window Object because is the nearest object that is attached to.
+
+function calculageAge(year) {
+  console.log(2016 - year);
+  console.log(this); // Output the window Object because is the nearest object that is attached to.
+}
+
+var john = {
+  name: "john",
+  yearOfBirth: 1988,
+  calculageAge: function() {
+    console.log(this); // Output the John Object because is the nearest object that is attached to.
+
+    function innerFunction() {
+      console.log(this); // Output the window Object because is the nearest object that is attached to. (is a function, not a method)
+    }
+    innerFunction();
+  }
+};
+
+john.calculageAge();
+
+var mike = {
+  name: 'mike',
+  yearOfBirth: 1984
+}
+
+mike.calculageAge = john.calculageAge;
+mike.calculageAge();
