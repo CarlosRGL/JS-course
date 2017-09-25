0ad8e872fa43a4b5b78c0822bfcf2d99e51f3ee5
CarlosRGL <carlos@quai13.com>
2017-09-25 17:39:58 +0200

    start PIG game

--- 03 DOM-Pig-Game/app.js
+++ 03 DOM-Pig-Game/app.js
@@ -8,3 +8,39 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+score = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+// hide dice from view at the start of the application and scores sets to 0
+document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+// add event listener to the button to change number when clicking
+document.querySelector(".btn-roll").addEventListener("click", function() {
+
+  // 1. calculate a number between 1 - 6
+  dice = Math.floor(Math.random() * 6 + 1);
+
+  // 2. calculate result
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+
+  // 3. update the score IF the dice was NOT 1
+  if (dice !== 1) {
+    // add score
+    roundScore += dice;
+    document.querySelector(".current-" + activePlayer).textContent = roundScore;
+  } else {
+    //next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+  }
+});
